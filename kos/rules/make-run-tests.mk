b83699a9e3ff528ad260957fd832807f3dcdca40
Georgii Matiukhin <georgii.matiukhin@auriga.com>
2022-05-16 15:48:54 +0300

    build: fix path to the test dir

--- kos/rules/make-run-tests.mk
+++ kos/rules/make-run-tests.mk
@@ -4,40 +4,40 @@
 
 .PHONY: helloworld
 helloworld:
-	$(Q)$(MAKE) run NODE_ARG='\"test/message/hello_world.js\"'
+	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/message/hello_world.js\"'
 
 .PHONY: bareminimim
 bareminimim:
-	$(Q)$(MAKE) run NODE_ARG='\"test/kos/bareminimim.js\"'
+	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/kos/bareminimim.js\"'
 
 .PHONY: consolestderr
 consolestderr:
-	$(Q)$(MAKE) run NODE_ARG='\"test/kos/console_stderr.js\"'
+	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/kos/console_stderr.js\"'
 
 .PHONY: pi
 pi:
-	$(Q)$(MAKE) run NODE_ARG='\"test/kos/pi_10000x25.js\"'
+	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/kos/pi_10000x25.js\"'
 
 .PHONY: sysinfo
 sysinfo:
-	$(Q)$(MAKE) run NODE_ARG='\"test/kos/sysinfo.js\"'
+	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/kos/sysinfo.js\"'
 
 .PHONY: http
 http:
-	$(Q)$(MAKE) run NODE_ARG='\"test/kos/http.js\"' NODE_PORT=$(NODE_PORT) \
-		QEMU_NET=1
+	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/kos/http.js\"' \
+		NODE_PORT=$(NODE_PORT) QEMU_NET=1
 
 .PHONY: crypto-classes
 crypto-classes:
-	$(Q)$(MAKE) run NODE_ARG='\"test/parallel/test-crypto-classes.js\"'
+	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/parallel/test-crypto-classes.js\"'
 
 .PHONY: crypto-hash
 crypto-hash:
-	$(Q)$(MAKE) run NODE_ARG='\"test/parallel/test-crypto-hash.js\"'
+	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/parallel/test-crypto-hash.js\"'
 
 .PHONY: test-addon
 test-addon:
-	$(Q)$(MAKE) run NODE_ARG='\"test/kos/test_addon.js\"'
+	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/kos/test_addon.js\"'
 
 .PHONY: tests
 tests: clean-arch
