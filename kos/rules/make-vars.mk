f202b46c3c674585a4edc7690bf4f55ccb13894a
Andrey Makarov <andrey.makarov@auriga.com>
2022-07-22 16:49:57 +0300

    build: address review comments

--- kos/rules/make-vars.mk
+++ kos/rules/make-vars.mk
@@ -24,10 +24,11 @@ SDK_VERSION  ?= 1.1.0.91
 DEST_CPU      = arm64
 ARCH_CFG_ARGS =
 QEMU          = qemu-system-aarch64
-# TODO: need to figure out proper args for aarch64 and arm32
-#       qemu since at the KOS-SDK it is used as '-machine virt'
-QEMU_OPTS     = -machine vexpress-a15,secure=on -cpu cortex-a72 \
-		-m 2048 -serial stdio -nographic -monitor none
+QEMU_OPTS     = -machine vexpress-a15,secure=on
+QEMU_OPTS    += -m 2048
+QEMU_OPTS    += -serial stdio
+QEMU_OPTS    += -nographic
+QEMU_OPTS    += -monitor none
 
 ifeq ($(strip $(QEMU_NET)),1)
 QEMU_OPTS += -nic user,hostfwd=tcp:127.0.0.1:${NODE_PORT}-10.0.2.10:8080
@@ -83,25 +84,6 @@ QEMU_OPTS += -S
 # enabling qemu listening incoming gdb connection
 QEMU_OPTS += -gdb tcp::$(GDB_SERVER_PORT)
 endif
-
-# Version comparison functions from https://stackoverflow.com/a/15637871
-# for checking QEMU_OPTS
-S :=
-# For non empty strings
-str.eq = $(if $(subst $1,,$2),$S,T)
-str.le = $(call str.eq,$(word 1,$(sort $1 $2)),$1)
-# Creates a list of digits from a number
-mklist = $(eval __tmp := $1)$(foreach i,0 1 2 3 4 5 6 7 8 9,$(eval __tmp := $$(subst $$i,$$i ,$(__tmp))))$(__tmp)
-shift = $(wordlist 2, $(words $1), $1)
-num.le = $(eval __tmp1 := $(call mklist,$1))$(eval __tmp2 := $(call mklist,$2))$(if $(call str.eq,$(words $(__tmp1)),$(words $(__tmp2))),$(call str.le,$1,$2),$(call str.le,$(words $(__tmp1)),$(words $(__tmp2))))
-#Strip zeroes from the beginning of a list
-list.strip = $(eval __flag := 1)$(foreach d,$1,$(if $(__flag),$(if $(subst 0,,$d),$(eval __flag :=)$d,$S),$d))
-# temp string: 0 - two number equals, L first LT, G first GT or second is short,
-gen.cmpstr = $(eval __Tmp1 := $(subst ., ,$1))$(eval __Tmp2 := $(subst ., ,$2))$(foreach i,$(__Tmp1),$(eval j := $(word 1,$(__Tmp2)))$(if $j,$(if $(call str.eq,$i,$j),0,$(if $(call num.le,$i,$j),L,G)),G)$(eval __Tmp2 := $$(call shift,$(__Tmp2))))$(if $(__Tmp2), L)
-ver.lt = $(call str.eq,$(word 1,$(call list.strip,$(call gen.cmpstr,$1,$2))),L)
-
-# Check that QEMU_OPTS contains -smp4 if needed:
-$(if $(call ver.lt,1.1.0.259,${SDK_VERSION}),$(if $(call findstring,-smp4,${QEMU_OPTS}),$(info QEMU_OPTS contains -smp4, OK),$(error QEMU_OPTS does not contain -smp4 though SDK_VERSION >= 1.1.0.260)))
 
 
 # ok, we're all set now. lets export build control variables to let
