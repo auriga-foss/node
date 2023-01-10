59d4f11de971a52107eed0ee1abcc622226b6251
Maksim Logaev <maksim.logaev@auriga.com>
2023-01-10 11:24:24 +0300

    Added build of examples for hw

--- kos/rules/make-run-tests.mk
+++ kos/rules/make-run-tests.mk
@@ -2,42 +2,48 @@
 # in one command (lazy approach)
 # This file is to be included into main Makefile
 
+override ACT = run
+
+ifeq ($(strip $(FOR_HW)),1)
+override ACT = clean-arch realhw
+endif
+
 .PHONY: helloworld
 helloworld:
-	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/message/hello_world.js\"'
+	$(Q)$(MAKE) $(ACT) NODE_ARG='\"/opt/node/test/message/hello_world.js\"'
 
 .PHONY: bareminimim
 bareminimim:
-	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/kos/bareminimim.js\"'
+	$(Q)$(MAKE) $(ACT) NODE_ARG='\"/opt/node/test/kos/bareminimim.js\"'
 
 .PHONY: consolestderr
 consolestderr:
-	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/kos/console_stderr.js\"'
+	$(Q)$(MAKE) $(ACT) NODE_ARG='\"/opt/node/test/kos/console_stderr.js\"'
 
 .PHONY: pi
 pi:
-	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/kos/pi_10000x25.js\"'
+	$(Q)$(MAKE) $(ACT) NODE_ARG='\"/opt/node/test/kos/pi_10000x25.js\"'
 
 .PHONY: sysinfo
 sysinfo:
-	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/kos/sysinfo.js\"'
+	$(Q)$(MAKE) $(ACT) NODE_ARG='\"/opt/node/test/kos/sysinfo.js\"'
 
 .PHONY: http
 http:
-	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/kos/http.js\"' \
+	$(Q)$(MAKE) $(ACT) NODE_ARG='\"/opt/node/test/kos/http.js\"' \
 		NODE_PORT=$(NODE_PORT) QEMU_NET=1
 
 .PHONY: crypto-classes
 crypto-classes:
-	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/parallel/test-crypto-classes.js\"'
+	$(Q)$(MAKE) $(ACT) NODE_ARG='\"/opt/node/test/parallel/test-crypto-classes.js\"'
 
 .PHONY: crypto-hash
 crypto-hash:
-	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/parallel/test-crypto-hash.js\"'
+	$(Q)$(MAKE) $(ACT) NODE_ARG='\"/opt/node/test/parallel/test-crypto-hash.js\"'
 
 .PHONY: test-addon
 test-addon:
-	$(Q)$(MAKE) run NODE_ARG='\"/opt/node/test/kos/test_addon.js\"'
+	$(Q)$(MAKE) $(ACT) NODE_ARG='\"/opt/node/test/kos/test_addon.js\"'
 
 .PHONY: tests
 tests: clean-arch
