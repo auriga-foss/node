ec1c126d0eda54373a37507e2cb669fe42a08d77
Maksim Logaev <maksim.logaev@auriga.com>
2023-01-11 18:20:17 +0300

    Fix path for test_addon

--- test/kos/test_addon.js
+++ test/kos/test_addon.js
@@ -3,7 +3,7 @@ require('../common');
 
 // The '.node' suffix will make nodejs process addon as a dynamic executable
 // file.
-const addon = require('/test/kos/addons/test_addon.node');
+const addon = require('/opt/node/test/kos/addons/test_addon.node');
 
 // File has to presist in the FS (even empty? so far we put '1' into the file)
 // and has to be 'discoverable by nodejs test file name code flow.
